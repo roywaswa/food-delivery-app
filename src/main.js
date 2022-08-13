c9f56be85078463566cb3cd722428899977ac485
roywaswa <roywaswa.rw@gmail.com>
2022-08-13 08:21:14 +0300

    add and configure apollo for graphql

--- src/main.js
+++ src/main.js
@@ -1,9 +1,28 @@
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
 import './style/main.scss'
 import App from './App.vue'
 import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { DefaultApolloClient } from '@vue/apollo-composable'
 
-const app = createApp(App)
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.MODE === 'development' ? 'http://localhost:1337/graphql' : import.meta.env.VITE_APP_GRAPHQL_ENDPOINT,
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, {
+      default: apolloClient,
+    })
+  },
+  render: () => h(App),
+})
 
 app.use(router)
 app.mount('#app')
