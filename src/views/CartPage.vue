0a0b466d91095cf8562bfaa246a47daed8de7129
roywaswa <roywaswa.rw@gmail.com>
2022-08-16 17:10:56 +0300

    cart listing

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -1,9 +1,16 @@
 <script setup>
 
+const cartItems = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+console.log(cartItems);
+
 </script>
 
 <template>
-  <h1>CART PAGE</h1>
+  <h1>CART</h1>
+  <div :key="item.id" v-for="item in cartItems" class="cartitem">
+    <h4>{{item.name}}</h4>
+    <p>{{item.quantity}} at Ksh.{{item.price}}</p>
+  </div>
 </template>
 
 
